1a0e67230644fb14a360526383279df3278dbcc7
Javier Grau <javiergrau.freelance@gmail.com>
2023-07-04 18:54:48 -0400

    Add Calculator.js & Calculator.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Calculator from './components/Calculator';
+import './components/Calculator.css';
 
 function App() {
   return (
     <div className="App">
+      <Calculator />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
